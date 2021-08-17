f8342e2d2ffb97bface4d186191b3affe67a7944
noman27 <blt.normanbruce@yahoo.com>
2021-08-17 17:47:34 +0600

    dsfds

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>UCREATE</title>
+    <meta charset="utf-8" >
+    <meta name="viewport" content="width=device-width , initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    
+
+
+<!------------------------------>
+    <link rel = "icon" type = "images/png" href = "images/logo_icon.png">
+    <link rel="stylesheet" href="css/f_style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
+     
+
+
+</head>
+        
+<body>
+    <header>
+        <img class="topLogo" src="images/logo3.png" alt="">
+        <nav>
+            <a href="#" class="toggle-button">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </a>
+            <ul class="nav_links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Dev Team</a></li>
+                <li><a href="#">Contact US</a></li>
+                <li><a class="LoginButton" href="#"><button>Sign In</button></a></li>
+                <li><a class="LoginButton" href="#"><button>Sign Up</button></a></li>
+            </ul>
+        </nav>    
+    </header>
+<!---------------------------------------------->
+
+
+<img src="home.png">
+<p>Hello World</p>
+<p>Hello World1</p>
+<p>Hello World2</p>
+<p>Hello World3</p>
+<p>Hello World4</p>
+
+
+
+
+<!----------------------------------------------->
+
+    <footer class="footer"> 
+        <div class="container">
+            <div class="row">
+                <div class="footer-col">
+                    <img class="topLogo" src="images/logoFooter.jpg" alt="">
+                </div>
+                <div class="footer-col">
+                    <h4>company</h4>
+                    <ul>
+                        <li><a href="#">about us</a></li>
+                        <li><a href="#">our services</a></li>
+                        <li><a href="#">privacy policy</a></li>
+                        <li><a href="#">affiliate program</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>get help</h4>
+                    <ul>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">returns</a></li>
+                        <li><a href="#">order status</a></li>
+                        <li><a href="#">payment options</a></li>
+                    </ul>
+                </div>
+                
+                <div class="footer-col">
+                    <h4>follow us</h4>
+						<div class="social-links">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+   </footer>
+</body>
+</html>
