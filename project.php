d01d6d3db791d509290ea69cc7cf67514e462ac0
noman27 <blt.normanbruce@yahoo.com>
2021-08-21 20:07:34 +0600

    project page added

---
+++ project.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>UCREATE-Projects</title>
+    <meta charset="utf-8">
+
+    <link rel = "icon" type = "images/png" href = "images/logo_icon.png">
+    <link rel="stylesheet"  type="text/css"  href="css/projectPageStyle.css">
+</head>
+<body>
+    
+</body>
+</html>
