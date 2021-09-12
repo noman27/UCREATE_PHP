1aa386d1a019f365c14b5ac241852b686c065ac7
noman27 <blt.normanbruce@yahoo.com>
2021-09-12 18:34:34 +0600

    Index page changed

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -50,10 +53,13 @@
             <div class="carousel-item">
                 <img src="images/Welcome_Ideas.png" class="d-block w-100" alt="...">
                 <h1 class="MidBannerTitle">GET NEW IDEAS AND SHARE IDEAS</h1>
-                <button class="learnMore">LEARN MORE</button>
+                <a href="#learnIdea2" id="learnIdea1"><button class="learnMore">LEARN MORE</button></a>
             </div>
             <div class="carousel-item">
-                <img src="images/Welcome_Ideas.png" class="d-block w-100" alt="...">
+                <img src="images/skill4.png" class="d-block w-100" alt="...">
+                <h1 class="lastTitle1">IMPROVE</h1>
+                <h1 class="lastTitle2">YOUR</h1>
+                <a href="#impskil2" id="#impskil1"><button class="learnMore">LEARN MORE</button></a>
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
@@ -66,10 +72,46 @@
         </button>
     </div>
 
-    <?php echo "Nigger?"?>
+    <div class="NewIdea" id="learnIdea2" onclick="window.location.hash='learnIdea1';">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="descript">
+                        <h3 class="innerTitles">GET NEW IDEAS AND SHARE IDEAS</h3>
+                        <p  class="innerDetails">In this platform ,you can demonstrate your project which is the manifastation of your idea.This might someone who
+                            need new idea for his/her project. UCREATE is the growing hive of ideas.As you surf through out the 
+                            site you will come across various unique projects that feed your brain with ideas.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="descImage">
+                        <img src="images/unlockIdea2.png" alt="new ideas">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    <div>
-        
+    <div class="improveSkill" id="impskil2" onclick="window.location.hash='impskil1';">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="descript">
+                        <h3 class="innerTitles">IMPROVE YOUR SKILL</h3>
+                        <p  class="innerDetails">People can visit your project and rate it. The rating will indicate how much unique your project is and
+                            how improvment you have to make.You can also communicate with experts who will give you proper direction to improvise your project 
+                            and also your skill.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="descImage">
+                        <img src="images/skillup2.png" alt="improve skill">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 
