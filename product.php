defca8329ebb21a2812ef2112e21822e4e0e3c38
SadeeMohib <180204066@aust.edu>
2021-10-02 12:59:33 +0600

    product page

---
+++ product.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>UCREATE-Projects</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width , initial-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet"  type="text/css"  href="css/bootstrap.min.css" >
+    <link rel = "icon" type = "images/png" href = "images/logo_icon.png">
+   
+	<link rel="stylesheet"  type="text/css"  href="css/style.css">
+    <link rel="stylesheet"  type="text/css"  href="css/productStyle.css">
+	
+    <link rel="stylesheet"  type="text/css"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+ 	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+ 	 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    
+</head>
+<body>
+
+	<header>
+    <?php include 'header.php'; ?>
+    </header>
+
+    <div class="container">
+		<div class="product-title">
+			<p>Product Title</p>
+		</div>
+		<!------------------slider------------------------->
+		
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1"></li>
+			</ol>
+			
+			<div class="carousel-inner">
+			<div class="item active">
+			 <img src="images/rtx.png" alt="">
+			</div>
+
+			<div class="item">
+			  <img src="images/rtx2.png" alt="">
+			</div>
+		  </div>
+		  
+		   <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left"></span>
+				<span class="sr-only">Previous</span>
+			  </a>
+			  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right"></span>
+				<span class="sr-only">Next</span>
+			  </a>
+			</div>
+		
+		
+		<div class="product-discription">
+			<p class="idea"> Idea :</p>
+			<p class="des">asbavgsd tgvsaygbduy asgbdaduyh auidhyuahdsuha asbavgsd tgvsaygbduy 
+			asgbdaduyh auidhyuahdsuhaasbavgsd tgvsaygbduy asgbdaduyh 
+			auidhyuahdsuhaasbavgsd tgvsaygbduy asgbdaduyh auidhyuahdsuha</p>
+		</div>
+			<div class="product-link">
+			<p>Link :</p>
+			
+		</div>
+			<a href="#"><button class="pre-btn">Previous</button></a>
+		<div class="line"></div>
+	
+		<div class="user-info">
+					<div class="col-md-8">	
+						
+						<div class="picture">
+								<img src="images/arts3.png"  alt="...">
+						</div>
+					
+					
+                        <p class="User-name">xyz saghdvga</p>
+						<!------------------------------------------>
+							<div class="wrapper">
+							<div class="button">
+							<div class="icon">
+								<i class="fab fa-facebook-f"></i></div>
+							<span>Facebook</span>
+							</div>
+							<div class="button">
+							<div class="icon"><i class="fab fa-twitter"></i></div>
+							<span>Twitter</span>
+							</div>
+							<div class="button">
+							<div class="icon">
+							<div class="gh">
+							<i class="fab fa-github"></i>
+							</div>
+							</div>
+							<span>Github</span>
+							</div>
+							</div>
+
+                        </div> 
+					</div>
+		</div>
+
+</body>
--- softUpload.php
+++ softUpload.php
@@ -6,34 +6,13 @@
     <meta name="viewport" content="width=device-width , initial-scale=1.0">
     <link rel = "icon" type = "images/png" href = "images/logo_icon.png">
     <link rel="stylesheet"  type="text/css"  href="css/bootstrap.min.css" >
-    <link rel="stylesheet"  type="text/css"  href="css/style.css">
 	<link rel="stylesheet"  type="text/css"  href="css/softStyle.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> 
     <link rel="stylesheet"  type="text/css"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     
 </head>
 <body>
-    <header>
-    <img class="topLogo" src="images/logo1.png" alt="">
-        <nav>
-            <a href="#" class="toggle-button">
-                <span class="bar"></span>
-                <span class="bar"></span>
-                <span class="bar"></span>
-            </a>
-            <ul class="nav_links">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="About.php">About</a></li>
-                <li><a href="project.php">Projects</a></li>
-                <li><a href="#">Dev Team</a></li>
-                <li><a href="contactUs.php">Contact US</a></li>
-                <li><a class="LoginButton" href="login.php"><button>Sign In</button></a></li>
-                <li><a class="LoginButton" href="register.php"><button>Sign Up</button></a></li>
-            </ul>
-        </nav>
-    </header>
-	
-			
+  	
 			<div class = "instruction"> <h3> What are you working on?</h3>
 			<p>Upload your design. This will also be used as the thumbnail in feeds.<p>
 			</div>
@@ -41,30 +20,41 @@
 			
 		
 		<div class="file__upload">
-		<div class="head">
-			<p><i class="fa fa-cloud-upload fa-2x"></i><span><span>up</span>load</span></p>			
+			<form class="midbody" action="#" method="POST" enctype="multipart/form-data">
+			 <div class="mb-3">
+                   <span class="details">Project Title :</span> 
+                   <input type="text" placeholder="Title of your project" name="title-name" required>
+				     <p> </p>
+              </div> 
+			  <div class="mb-3">
+                   <span class="details">Link :</span>
+                   <input type="text" placeholder="Link of your project" name="link-name" required>
+				 
+              </div>
+				<div class="genre">
+				<h2>Project type :</h2>
+				<ul class="tags">
+				  <li><a href="#" class="tag">Software</a></li>
+				  <li><a href="#" class="tag">Hardware</a></li>
+				  <li><a href="#" class="tag">Arts</a></li>
+				</ul>
+				</div>
+				<div class="description">
+				<textarea placeholder="Say something about your project" class="field"></textarea>
+				</div>
+				<h4>Upload your projects picture</h4>
+				<div class="ss-upload"
+					<ul>
+					<li><input type="file" id="image_uploads" name="image_uploads" accept=".jpg, .jpeg, .png" multiple></li>
+					<li><input type="file" id="image_uploads2" name="image_uploads2" accept=".jpg, .jpeg, .png" multiple></li>
+					</ul>
+					<a href=""><button type="submit" >Upload</button></a>
+
+				</div>		
+			
+			</form>
+			<a href="index.php"><button >Home</button></a>
 		</div>
-		<form class="midbody">
-			<input type="file" id="upload">
-			<label for="upload">
-				<i class="fa fa-file-text-o fa-3x"></i>
-				<p>
-					<strong>Drag and drop</strong> files here<br>
-					or <span>browse</span> to begin the upload
-				</p>
-			</label>
-		</form>
-	</div>
-		<div class = "description">
-			<div class = "message">
-			<textarea rows="6" cols="85" placeholder="  Write what went into this shot,and anything else you would like to mention" class="field"></textarea>
-			</div>
-		</div>
-		
-		<div id="outer">
-				<div class="inner"><button class="msgBtnBack">Back</button></div>
-				<div class="inner"><button type="submit" class="msgBtn" onClick="return false;" >Save</button></div>		
-			</div>
     <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
